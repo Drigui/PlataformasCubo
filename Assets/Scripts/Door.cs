bff148b6c257a7cd516885f3a0840eea8c426e19
Drigui <rlopezhidalgo@hotmail.com>
2024-11-17 12:36:04 +0100

    changetextcolor

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
     public string sceneName;
+    public GameObject backMenuBt;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +30,8 @@ public class Door : MonoBehaviour
         else if (LevelManager.Instance.hasKey && SceneManager.GetActiveScene().name == GameConstant.LEVEL3_KEY)
         {
             LevelManager.Instance.winPanel.SetActive(true);
+            EventSystem.current.firstSelectedGameObject = backMenuBt;
+            EventSystem.current.SetSelectedGameObject(backMenuBt);
         }
     }
 }
--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -12,23 +12,40 @@ public class MenuManager : MonoBehaviour
     public GameObject selectPanel;
     public GameObject quitPanel;
     public Button button;
-    public TMP_Text startText;
+    public GameObject currentBt;
 
+    //first selected buttons
+    public GameObject startBt;
+    public GameObject lvl1Bt;
+    public GameObject yesBt;
+
+    public TMP_Text text;
+    public List<TMP_Text> listaTexts = new List<TMP_Text>();
+
+    public GameObject contenedorLista;
 
     
 
 
     private void Awake()
     {
+        contenedorLista = EventSystem.current.firstSelectedGameObject.transform.parent.gameObject;
         mainPanel.SetActive(true);
         selectPanel.SetActive(false);
         quitPanel.SetActive(false);
+        foreach (TMP_Text childText in contenedorLista.GetComponentsInChildren<TMP_Text>())
+        {
+            listaTexts.Add(childText);
+
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentBt = EventSystem.current.currentSelectedGameObject.gameObject;
+        ChangeTextColor();
     }
     public void ChangeScene(string scename)
     {
@@ -38,6 +55,11 @@ public class MenuManager : MonoBehaviour
     {
         mainPanel.SetActive(false);
         selectPanel.SetActive(true);
+        //EventSystem.current.firstSelectedGameObject = lvl1Bt;
+        EventSystem.current.SetSelectedGameObject(lvl1Bt);
+        DeleteList();
+        CheckList();
+       
 
     }
     public void ChangeQuitPanel()
@@ -45,6 +67,10 @@ public class MenuManager : MonoBehaviour
         mainPanel.SetActive(false);
         selectPanel.SetActive(false);
         quitPanel.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(yesBt);
+        DeleteList();
+        CheckList();
+
 
     }
     public void CloseQuitPanel()
@@ -52,10 +78,54 @@ public class MenuManager : MonoBehaviour
         mainPanel.SetActive(true);
         selectPanel.SetActive(false);
         quitPanel.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(startBt);
+        DeleteList();
+        CheckList();
+
+
 
     }
     public void Exit()
     {
         Application.Quit();
+    }
+
+    public void CheckList()
+    {
+        contenedorLista = EventSystem.current.currentSelectedGameObject.transform.parent.gameObject;
+        foreach (TMP_Text childText in contenedorLista.GetComponentsInChildren<TMP_Text>())
+        {
+            listaTexts.Add(childText);
+
+        }
+    }
+    public void DeleteList()
+    {
+        listaTexts.Clear();
+
+    }
+    public void ChangeTextColor()
+    {
+        foreach (TMP_Text obj in listaTexts)
+        {
+
+            for (int i = 0; i < currentBt.GetComponentsInChildren<TMP_Text>().Length; i++)
+            {
+                if (obj == currentBt.GetComponentInChildren<TMP_Text>())
+                {
+                    Debug.Log(obj);
+
+                    text = currentBt.GetComponentInChildren<TMP_Text>();
+                    text.color = Color.white;
+                }
+                else if (obj != currentBt.GetComponentInChildren<TMP_Text>())
+                {
+
+                    //text = currentBt.GetComponentInChildren<TMP_Text>();
+
+                    obj.color = Color.black;
+                }
+            }
+        }
     }
 }
